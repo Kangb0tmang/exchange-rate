dea719701ce519fd0ea608a9cf759443c265223f
Kangb0tmang <Jon.Kang.JD@gmail.com>
2021-06-22 17:31:23 +1000

    useState hook

--- src/components/AmountField.js
+++ src/components/AmountField.js
@@ -1,29 +1,30 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { getAmount } from "../store/reducers/RateReducer";
-import { amountChanged } from "../store/actions/RateActions";
-import { debounce } from "lodash";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getAmount } from '../store/reducers/RateReducer';
+import { amountChanged } from '../store/actions/RateActions';
+import { debounce } from 'lodash';
 
-export class AmountField extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { amount: props.amount };
-    this.onChange = this.onChange.bind(this);
-    this.changeAmount = debounce(this.props.changeAmount, 500);
-  }
-  onChange(e) {
+export function AmountField({ amount, changeAmount }) {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { amount: props.amount };
+  //   this.onChange = this.onChange.bind(this);
+  //   this.changeAmount = debounce(this.props.changeAmount, 500);
+  // }
+
+  const [displayAmount, setDisplayAmount] = useState(amount);
+
+  function onChange(e) {
     let newAmount = e.target.value;
-    this.setState({ amount: newAmount });
-    this.changeAmount(newAmount);
+    setDisplayAmount(newAmount);
+    // this.changeAmount(newAmount);
   }
-  render() {
-    return (
-      <form className="ExchangeRate-form">
-        <input type="text" value={this.state.amount} onChange={this.onChange} />
-      </form>
-    );
-  }
+  return (
+    <form className='ExchangeRate-form'>
+      <input type='text' value={displayAmount} onChange={onChange} />
+    </form>
+  );
 }
 
 // prop types
