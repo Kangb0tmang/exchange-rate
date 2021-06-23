21d71c0f9312f6a73e8bb1082ac4edc6179c7927
Kangb0tmang <Jon.Kang.JD@gmail.com>
2021-06-23 14:30:38 +1000

    useEffect hook

--- src/components/ExchangeRate.js
+++ src/components/ExchangeRate.js
@@ -1,49 +1,46 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { ratesUpdated } from "../store/actions/RateActions";
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { ratesUpdated } from '../store/actions/RateActions';
 import {
   getCurrencyCode,
   getSupportedCurrencies,
-} from "../store/reducers/RateReducer";
-import { RateTableContainer } from "./RateTable";
-import { CurrencyCodePickerContainer } from "./CurrencyCodePicker";
-import { getExchangeRates } from "../api";
-import { AmountFieldContainer } from "./AmountField";
+} from '../store/reducers/RateReducer';
+import { RateTableContainer } from './RateTable';
+import { CurrencyCodePickerContainer } from './CurrencyCodePicker';
+import { getExchangeRates } from '../api';
+import { AmountFieldContainer } from './AmountField';
 
-export class ExchangeRate extends React.Component {
-  constructor(props) {
-    super(props);
-    this.getLatestExchangeRates();
-  }
-  componentDidUpdate(prevProps) {
-    if (this.props.currencyCode !== prevProps.currencyCode) {
-      this.getLatestExchangeRates();
-    }
-  }
-  getLatestExchangeRates() {
-    const { currencyCode, updateRates, supportedCurrencies } = this.props;
+export function ExchangeRate({
+  currencyCode,
+  updateRates,
+  supportedCurrencies,
+}) {
+  // Runs only on first time component is rendered
+  useEffect(() => {
+    getLatestExchangeRates();
+  }, [currencyCode]);
+
+  function getLatestExchangeRates() {
     getExchangeRates(currencyCode, supportedCurrencies).then((rates) => {
       updateRates(rates);
     });
   }
-  render() {
-    return (
-      <>
-        <section>
-          <h1 className="ExchangeRate-header">
-            Exchange Rates <CurrencyCodePickerContainer />
-          </h1>
-        </section>
-        <section>
-          <AmountFieldContainer />
-        </section>
-        <section>
-          <RateTableContainer />
-        </section>
-      </>
-    );
-  }
+  return (
+    <>
+      <section>
+        <h1 className='ExchangeRate-header'>
+          Exchange Rates <CurrencyCodePickerContainer />
+        </h1>
+      </section>
+      <section>
+        <AmountFieldContainer />
+      </section>
+      <section>
+        <RateTableContainer />
+      </section>
+    </>
+  );
 }
 
 // props types
